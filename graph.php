b152eea909fe1eaa6249c10bb3892d6e49c7cf0e
user <user@user.com>
2016-09-27 00:17:06 -0400

    Initial commit

---
+++ graph.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$_SESSION['page'] = "graph";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/over.css" rel="stylesheet">
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<title>CDC SensorView</title>
+	</head>
+	<body>
+	<?php include("nav.php"); ?>
+	</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+function is_active_page($page_name) {
+	if ($_SESSION['page'] == $page_name) {
+		echo " class=\"active\"";
+	} else {
+		echo "";
+	}
+}
+?>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+	  <a class="navbar-brand" href="#">CDC SensorView</a>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+		<ul class="nav navbar-nav navbar-right">
+			<li <?php is_active_page("table"); ?>><a href="index.php">Table View</a></li>
+			<li <?php is_active_page("graph"); ?>><a href="graph.php">Graph View</a></li>
+		</ul>
+	</div>
+  </div>
+</nav>
