dd7966f2a73983d8c45a1487f38e1d65a36bab07
user <user@user.com>
2016-10-18 19:10:44 -0400

    Began work on search parameters
    Added datepicker tool

--- index.php
+++ index.php
@@ -22,9 +22,20 @@ function make_row($row) {
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-datetimepicker.css" rel="stylesheet">
 		<link href="css/over.css" rel="stylesheet">
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/moment-with-locales.js"></script>
+		<script src="js/bootstrap-datetimepicker.js"></script>
+		<script type="text/javascript">
+			$(function () {
+				$('#from_date').datetimepicker();
+			});
+			$(function () {
+				$('#to_date').datetimepicker();
+			});
+		</script>
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -37,7 +48,46 @@ function make_row($row) {
 	<div class="container">
 		<div class="panel panel-default">
 			<div class="panel-heading"><h2>Search Parameters</h2></div>
-			<div class="panel-body">Panel Content</div>
+			<div class="panel-body">
+				<form>
+				<div class="row">
+				<div class="col-md-4">
+					<div class="radio">
+					  <label><input type="radio" name="search" id="time">Time Range</label>
+					</div>
+					From:         
+					<div class='input-group date' id='from_date'>
+						<input type='text' class="form-control" />
+						<span class="input-group-addon">
+							<span class="glyphicon glyphicon-calendar"></span>
+						</span>
+					</div>
+
+					To: 
+					<div class='input-group date' id='to_date'>
+						<input type='text' class="form-control" id="to_date_input"/>
+						<span class="input-group-addon">
+							<span class="glyphicon glyphicon-calendar"></span>
+						</span>
+					</div>
+				</div>
+				
+				<div class="col-md-4">
+					<div class="radio">
+						<label><input type="radio" name="search" id="node">Node ID</label>
+					</div>
+				</div>
+				
+				<div class="col-md-4">
+					<div class="radio">
+						<label><input type="radio" name="search" id="dust">Dust Range</label>
+					</div>
+				</div>
+				</div>
+				<br>
+				<button type="submit" class="btn btn-default btn-lg">Submit</button>
+				</form>
+			</div>
 		</div>
 
 		<div class="panel panel-default">
@@ -55,7 +105,7 @@ function make_row($row) {
 				</thead>
 				<tbody>
 					<?php 
-					if ($result = $db_con->query("SELECT * FROM data")) {
+					if ($result = $db_con->query("SELECT * FROM data LIMIT 20")) {
 						while ($curr_row = $result->fetch_assoc()) {
 							echo make_row($curr_row);
 						}
