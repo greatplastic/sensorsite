eabd366b69e3b794d91ffe6a216db13c9e904516
user <user@user.com>
2016-11-21 23:36:14 -0500

    Graphing MySQL data, scaling needed

--- graph.php
+++ graph.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
-date_default_timezone_set('America/New_York'); // EST for date conversions
-$_SESSION['page'] = "graph";
 include("db.php");
 include("search_params.php");
+date_default_timezone_set('America/New_York'); // EST for date conversions
+$_SESSION['page'] = "graph";
 require_once ('jpgraph/jpgraph.php');
 require_once ('jpgraph/jpgraph_line.php');
+require_once ('jpgraph/jpgraph_date.php');
 require_once ('jpgraph/jpgraph_error.php');
 $db = new DBManager();
 $result = NULL;
@@ -13,8 +14,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$sp = new SearchParameters();
 	$sp->set_time_params($_POST["from_time_input"], $_POST["to_time_input"]);
 	$sp->set_node_params($_POST["node_input"]);
-	var_dump($sp);
 	$result = $db->execute($sp);
+}
+
+function make_base_graph($title, $x_data) {
+	$graph = new Graph(800,600);
+	$graph->SetScale("datlin");
+	$theme_class = new UniversalTheme;
+	$graph->SetTheme($theme_class);
+	$graph->img->SetAntiAliasing(false);
+	$graph->title->Set($title);
+	$graph->SetBox(false);
+	$graph->img->SetAntiAliasing();
+	
+	$graph->yaxis->HideZeroLabel();
+	$graph->yaxis->HideLine(false);
+	$graph->yaxis->HideTicks(false,false);
+	
+	$graph->xgrid->Show();
+	$graph->xgrid->SetLineStyle("solid");
+	$graph->xaxis->SetTickLabels($x_data);
+	$graph->xgrid->SetColor('#E3E3E3');
+	return $graph;
 }
 ?>
 
@@ -85,61 +106,66 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				</form>
 			</div>
 		</div>
-<?php
-$datay1 = array(20,15,23,15);
-$datay2 = array(12,9,42,8);
-$datay3 = array(5,17,32,24);
+		<?php
+		$time_data = array();
+		$temp_data = array();
+		$hum_data = array();
+		$dust_data = array();
+		if (!is_null($result)) {
+			$i = 0;
+			while (($curr_row = $result->fetch_assoc()) && $i < 7) {
+				$time_data[] = date("Y-m-d H:i:s", strtotime($curr_row["timestamp"]));
+				$temp_data[] = $curr_row["temperature"];
+				$hum_data[] = $curr_row["humidity"];
+				$dust_data[] = $curr_row["dust"];
+				$i++;
+			}
+			$result->free();
+			$temp_graph = make_base_graph("Temperature", $time_data);
+			$hum_graph = make_base_graph("Humidity", $time_data);
+			$dust_graph = make_base_graph("Dust", $time_data);
+			
+			// Filter data length here
+			
 
-// Setup the graph
-$graph = new Graph(800,600);
-$graph->SetScale("textlin");
+			$p1 = new LinePlot($temp_data);
+			$temp_graph->Add($p1);
+			$p1->SetColor("#6495ED");
+			$p1->SetLegend('Temperature');
 
-$theme_class=new UniversalTheme;
+			$p2 = new LinePlot($hum_data);
+			$hum_graph->Add($p2);
+			$p2->SetColor("#B22222");
+			$p2->SetLegend('Humidity');
 
-$graph->SetTheme($theme_class);
-$graph->img->SetAntiAliasing(false);
-$graph->title->Set('Filled Y-grid');
-$graph->SetBox(false);
+			$p3 = new LinePlot($dust_data);
+			$dust_graph->Add($p3);
+			$p3->SetColor("#FF1493");
+			$p3->SetLegend('Dust');
 
-$graph->img->SetAntiAliasing();
+			$temp_graph->legend->SetFrameWeight(1);
+			$hum_graph->legend->SetFrameWeight(1);
+			$dust_graph->legend->SetFrameWeight(1);
 
-$graph->yaxis->HideZeroLabel();
-$graph->yaxis->HideLine(false);
-$graph->yaxis->HideTicks(false,false);
-
-$graph->xgrid->Show();
-$graph->xgrid->SetLineStyle("solid");
-$graph->xaxis->SetTickLabels(array('A','B','C','D'));
-$graph->xgrid->SetColor('#E3E3E3');
-
-// Create the first line
-$p1 = new LinePlot($datay1);
-$graph->Add($p1);
-$p1->SetColor("#6495ED");
-$p1->SetLegend('Line 1');
-
-// Create the second line
-$p2 = new LinePlot($datay2);
-$graph->Add($p2);
-$p2->SetColor("#B22222");
-$p2->SetLegend('Line 2');
-
-// Create the third line
-$p3 = new LinePlot($datay3);
-$graph->Add($p3);
-$p3->SetColor("#FF1493");
-$p3->SetLegend('Line 3');
-
-$graph->legend->SetFrameWeight(1);
-
-// Output line
-@unlink("test.jpg");
-$graph->Stroke("test.jpg");
-?>
+			// Output line
+			@unlink("temp_graph.jpg");
+			@unlink("hum_graph.jpg");
+			@unlink("dust_graph.jpg");
+			$temp_graph->Stroke("temp_graph.jpg");
+			$hum_graph->Stroke("hum_graph.jpg");
+			$dust_graph->Stroke("dust_graph.jpg");
+		}
+		?>
 		<div class="panel panel-default">
 			<div class="panel-heading"><h2>Results</h2></div>
 			<div class="panel-body">
-			<img src="test.jpg"></img>
+			<?php
+			if (!is_null($result)) {
+				echo '<img src="temp_graph.jpg"></img><br>';
+				echo '<img src="hum_graph.jpg"></img><br>';
+				echo '<img src="dust_graph.jpg"></img><br>';
+			}
+			?>
 			</div>
 		</div>
 	</div>
